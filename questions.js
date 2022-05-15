ea82e4d39457bce11338e193a675d50117e33525
Romain L <51294979+TisoOfficiel@users.noreply.github.com>
2022-05-15 19:33:55 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,171 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quel Streamer à fait revenir Locklear sur la scène du stream",
+    answer: "A : Alphacast",
+    options: [
+      "A : Alphacast",
+      "B : AlphacastPas",
+      "C : Squeezie",
+      "D : Domingo",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Sous quelle casquette Locklear est-il connu",
+    answer: "D : MLG",
+    options: [
+      "A : NZXT #AD",
+      "B : NY",
+      "C : Aucune il ne porte pas de casquette",
+      "D : MLG",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Avec quel champion Overwath de Locklear et le meilleur du monde",
+    answer: "B : Zarya",
+    options: [
+      "A : Soldat 76",
+      "B : Zarya",
+      "C : Ana",
+      "D : Sombra",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Locklear incarne à ses heures perdues un agent secret qui est-ce ?",
+    answer: "D : L'agent 107",
+    options: [
+      "A : L'agent OSS 117",
+      "B : L'agent 007",
+      "C : L'agent 777",
+      "D : L'agent 107",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Sur quel jeu Locklear est-il champion d'Europe",
+    answer: "C : Sea of Thieves",
+    options: [
+      "A : Tous",
+      "B : Battlefield",
+      "C : Sea of Thieves",
+      "D : Aucun c'est un noobs",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  {
+  numb: 6,
+  question: "Avec quel équipage Locklear forme la LockBiZy",
+  answer: "A : Squeezie et Doigby",
+  options: [
+      "A : Squeezie et Doigby",
+      "B : Inox et Michou",
+      "C : Jiraya et Xari",
+      "D : Gotaga et Mickalow",
+  ]
+  },
+  {
+  numb: 7,
+  question: "Quel sport pratique Locklear de façon très régulière depuis son installation à Malte",
+  answer: "D : Le surf",
+  options: [
+    "A : La randonnée",
+    "B : La pétanque",
+    "C : Aucun c'est une feignass",
+    "D : Le surf",
+  ]
+  },
+  {
+  numb: 8,
+  question: "Combien de participation physique à fait Locklear au Zevent",
+  answer: "B : 3",
+  options: [
+    "A : 1",
+    "B : 3",
+    "C : 5",
+    "D : 4",
+  ]
+  },
+  {
+  numb: 9,
+  question: "Pour quel raison en 2018 Locklear s'est fait viré du Zevent",
+  answer: "B : A cause de l'utilisation abusive de sa flûte",
+  options: [
+    "A : Car il crit trop",
+    "B : A cause de l'utilisation abusive de sa flûte",
+    "C : Pour avoir fait une danse Fortnite",
+    "D : Car il bouge trop",
+  ]
+  },
+  {
+  numb: 10,
+  question: "Lors de quel édition du Zevent Locklear à du transformer sa Tesla en Hello Kitty",
+  answer: "B : L'édition de 2019",
+  options: [
+    "A : L'édition de 2018",
+    "B : L'édition de 2019",
+    "C : L'édition de 2020",
+    "D : L'édition de 2021",
+  ]
+  },
+  {
+  numb: 11,
+  question: "Quelle expression est très souvent utilisé",
+  answer:  "A : Bigs Noobs !",
+  options: [
+    "A : Bigs Noobs !",
+    "B : GG à toi tu as été meilleur",
+    "C : Gotaga est meilleur que moi",
+    "D : Vous êtes les meilleurs Boom",
+  ]
+  },
+  {
+  numb: 12,
+  question: "Lors d'un Zevent Locklear à 'réussie' à séduire une streameuse qui était-elle ?",
+  answer: "B : Jeel",
+  options: [
+    "A  Maghla",
+    "B : Jeel",
+    "C : Ultia",
+    "D : Toutes c'est un tombeur",
+  ]
+  },
+  {
+  numb: 13,
+  question: "Sur quel jeu Locklear à battu Gotaga the French Monster en 1v1",
+  answer:  "B : Bowling sur Switch",
+  options: [
+    "A : Wii Sport",
+    "B : Bowling sur Switch",
+    "C : Call Of Duty",
+    "D : Battlefield",
+  ]
+  },
+  {
+  numb: 14,
+  question: "De quel univer vient le pseudo Locklear",
+  answer: "C : Halo",
+  options: [
+    "A : Overwatch",
+    "B : Battlefield",
+    "C : Halo",
+    "D : World of Warcraft",
+  ]
+  },
+  {
+  numb: 15,
+  question: "Quel étude à fait Locklear",
+  answer: "A : Motion Designer",
+  options: [
+      "A : Motion Designer",
+      "B : Cringe Ingenior",
+      "C : Pro gamer",
+      "D : Master en informatiquer",
+  ]
+  },
+];
